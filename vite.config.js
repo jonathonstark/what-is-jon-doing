26bdb7dc5e8816111c2d79c3070db2a6f5559e7a
jonathonstark <65309830+jonathonstark@users.noreply.github.com>
2026-07-20 08:02:05 -0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// Replace 'your-repo-name' with your actual GitHub repository name
-export default defineConfig({
-  plugins: [react()],
-  base: '/what-is-jon-doing/',
-});
